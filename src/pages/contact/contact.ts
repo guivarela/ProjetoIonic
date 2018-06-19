2d64f8b14a4376a2997c5f074a29c7e9ea28613b
guivarela <guivarela7@hotmail.com>
2018-06-18 22:01:10 -0300

    header painel fixed

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 //import { ItemPainel } from '../../Model/ItemPainel';
 import { SenhasProvider } from '../../providers/senhas/senhas';
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the ListaPaisesPage page.
@@ -20,15 +21,31 @@ import { SenhasProvider } from '../../providers/senhas/senhas';
 })
 export class ContactPage {
 	public items = Array<any>();
+	private task;
 	public continente;
 
 	constructor(public navCtrl: NavController, public navParams: NavParams, public senhasProvider: SenhasProvider) {
-
+		this.task = setInterval(() => {
+			this.carregaPainel();
+		}, 100000);
   }
 	//irParaDestino(pais: Pais): void {
 	//	this.navCtrl.push(DetalhePage, { paisSelecionado: pais });
 	//}
-	
+
+	carregaPainel():void{		
+		this.senhasProvider.getPainel().subscribe(
+		  data => {
+			const response = (data as any);
+			const objeto_retorno = JSON.parse(response._body);
+			this.items = objeto_retorno;
+			console.log(response);
+		  }, error => {
+			console.log(error)
+		  }
+		)
+		console.log('ionViewDidLoad ContactPage');
+	}
 
 	ionViewDidLoad() {
 		this.senhasProvider.getPainel().subscribe(
